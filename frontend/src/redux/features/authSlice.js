d38b949883460dc486ab1d2fc50f686909e4ea1f
chathuwa-whiz <navodchathushka@gmail.com>
2025-05-15 10:36:36 +0530

    initial setup

---
+++ frontend/src/redux/features/authSlice.js
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const authApi = createApi({
+    reducerPath: 'authApi',
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: 'http://localhost:5000/api',
+        prepareHeaders: (headers) => {
+            const token = localStorage.getItem('token');
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`);
+            }
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (credentials) => ({
+                url: '/users/login',
+                method: 'POST',
+                body: credentials,
+            }),
+        }),
+        register: builder.mutation({
+            query: (userData) => ({
+                url: '/users/register',
+                method: 'POST',
+                body: userData,
+            }),
+        }),
+        fetchUserProfile: builder.query({
+            query: () => '/users/profile',
+        }),
+        updateUserProfile: builder.mutation({
+            query: (userData) => ({
+                url: '/users/profile',
+                method: 'PUT',
+                body: userData,
+            }),
+        }),
+    }),
+});
+
+export const { 
+    useLoginMutation, 
+    useRegisterMutation, 
+    useFetchUserProfileQuery,
+    useUpdateUserProfileMutation
+} = authApi;
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+    reducer: {
+        
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware(),
+})
+
+setupListeners(store.dispatch);
